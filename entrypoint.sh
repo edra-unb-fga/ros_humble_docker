df17a94f046da11fdf925879f1fa205fe4b341a6
Matheus Rodrigues <102708746+mrodrigues14@users.noreply.github.com>
2025-05-23 00:15:58 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-# Configure o ambiente ROS2
+# Ativa o ambiente ROS 2
 source /opt/ros/humble/setup.bash
+
+# Faz o build se ainda não existir o setup
+if [ ! -f /root/ros2_ws/install/local_setup.bash ]; then
+    echo "[INFO] colcon build ainda não foi executado. Iniciando build..."
+    cd /root/ros2_ws
+    colcon build
+fi
+
+# Ativa o ambiente do workspace
 source /root/ros2_ws/install/local_setup.bash
 
-# Execute o nó ROS2
+# Executa o comando passado no CMD
 exec "$@"
